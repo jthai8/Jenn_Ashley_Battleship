4e606f6b4076f56abf406934e14359592a0bd1a9
AJ <ashley04jen@gmail.com>
2017-02-07 14:24:34 -0800

    safety commit

--- battleshipmodel.js
+++ battleshipmodel.js
@@ -1,1 +1,26 @@
-  var count = 1;
+var count = 1;
+var board =[
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0]
+];
+var ship = 1;
+
+function placeShips() {
+  // random row
+  var boardRow = Math.floor(Math.random()*9 +1);
+  // random column
+  var boardCol = Math.floor(Math.random()*9 +1);
+  board[boardRow][boardCol]= ship;
+  console.log(board);
+  // loop through board; if a ship exists at board[row][column], keep going
+  // if not, board[row][column] = ship
+  // do this until five ships are placed
+};
