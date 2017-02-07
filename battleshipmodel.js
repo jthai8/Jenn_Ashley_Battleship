a239e8b4eaea302d607e11171f412762c941049a
AJ <ashley04jen@gmail.com>
2017-02-07 15:23:35 -0800

    commit fifth story

--- battleshipmodel.js
+++ battleshipmodel.js
@@ -17,13 +17,15 @@ function placeShips() {
   // loop through board; if a ship exists at board[row][column], keep going
   //create a for loop that places a ship at a random position, if spot taken, keep going
   //vv start of ship placement for loop
-  for(var i=0; i <= 5; i++) {
+  for(var i=0; i < 5; i++) {
     // generates random row
     var boardRow = Math.floor(Math.random()*9);
     // generates random column
     var boardCol = Math.floor(Math.random()*9);
     //assign spot with ship value = 1
-    board[boardRow][boardCol]= ship;
+    if(board[boardRow][boardCol] == 0 ){
+      board[boardRow][boardCol]= ship;
+    };
   //^^end of ship placement for loop
 
   };
