0a70d33ec32b815c16dfcd541ad407d40a24807a
AJ <ashley04jen@gmail.com>
2017-02-08 16:45:06 -0800

    Second Epic, Second Story

--- battleshipmodel.js
+++ battleshipmodel.js
@@ -15,33 +15,47 @@ var board =[
 var ship = 1;
 var sum;
 var shipLoc =[];
+function spaces(bRow, bCol) {
+  if ((bRow == 0 || board[bRow-1][bCol] == 0) &&
+      (bRow == 9 || board[bRow+1][bCol] == 0) &&
+      (bCol == 0 || board[bRow][bCol-1] == 0) &&
+      (bCol == 9 || board[bRow][bCol+1] == 0)) {
+      // (board[bRow][bCol] == undefined)) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
 function placeShips() {
   // loop through board; if a ship exists at board[row][column], keep going
   //create a for loop that places a ship at a random position, if spot taken, keep going
+
   //vv start of ship placement for loop
-  for(var i=0; i < 5; i++) {
+  while(shipLoc.length<5) {
     // generates random row
     var bRow = Math.floor(Math.random()*9);
     // generates random column
     var bCol = Math.floor(Math.random()*9);
     //assign spot with ship value = 1
-    if(board[bRow][bCol] == 0 ){
+    if(spaces(bRow, bCol) && board[bRow][bCol] == 0){
       board[bRow][bCol]= ship;
       shipLoc.push(bRow.toString() + bCol.toString());
-    };
+    }
+  }
   //^^end of ship placement for loop
-  var d = board[0].concat(board[1],board[2],board[3],board[4],board[5],board[6],board[7],board[8],board[9]);
 
-  sum = d.reduce(add,0);
-
-  function add(a,b){
-    return a+b;
-  };
-
-  };
+};
 
   console.log(board);
   // if not, board[row][column] = ship
   // do this until five ships are placed
-};
+
+
+// var d = board[0].concat(board[1],board[2],board[3],board[4],board[5],board[6],board[7],board[8],board[9]);
+//
+// sum = d.reduce(add,0);
+//
+// function add(a,b){
+//   return a+b;
+// };
